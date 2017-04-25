658abd9aefc0d7abce4b0bac9ae2c7613378dcb3
NikitaMelnyk <nickwtwfu@gmail.com>
2017-04-25 17:51:13 +0300

    refactoring

--- src/test/java/pages/Formula1Page.java
+++ src/test/java/pages/Formula1Page.java
@@ -7,6 +7,11 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import ru.yandex.qatools.allure.annotations.Step;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Period;
 import java.util.List;
 
 public class Formula1Page extends AbstractPage<Formula1Page> {
@@ -15,12 +20,10 @@ public class Formula1Page extends AbstractPage<Formula1Page> {
     private List<WebElement> listWithTweetItems;
     @FindBy(how = How.XPATH, using = "//li[contains(@id, 'stream-item-tweet')]//button[1]")
     private WebElement buttonRetweet;
-    @FindBy(how = How.XPATH, using = "//*[@id=\"retweet-tweet-dialog-dialog\"]/div[2]/form/div[2]/div[3]")
+    @FindBy(how = How.XPATH, using = "//*[@id=\"retweet-tweet-dialog-dialog\"]/div[2]/form/div[2]/div[3]/button")
     private WebElement buttonRetweetOnDialog;
 
     private By buttonRetweetLocator = new By.ByXPath(".//div[2]/div[2]/button[1]");
-
-    public List<WebElement> allItems;
 
     public Formula1Page(WebDriver driver) {
         super(driver);
@@ -40,41 +43,32 @@ public class Formula1Page extends AbstractPage<Formula1Page> {
                 scrollToElement(buttonRetweet);
                 System.out.println("Scroll to Element");
 
-                if (listWithTweetItems.get(i)
-                        .findElement(new By.ByXPath
-                                (".//span[contains(@class,'_timestamp')]"))
-                        .isEnabled())
-                {
+                LocalDate date = LocalDate.from(LocalDateTime.of(2017, Month.APRIL,23,13, 58));
+
+                Timestamp timestamp = getTweetTimestamp(listWithTweetItems.get(i));
+                LocalDate localDate = timestamp.toLocalDateTime().toLocalDate();
+                System.out.println(timestamp);
+
+                Period period = Period.between(date, localDate);
+
+                if (!period.isNegative()) {
                     clickElement(listWithTweetItems.get(i).findElement(buttonRetweetLocator));
                     System.out.println("click ON button Retweet is done...");
 
                     clickElement(buttonRetweetOnDialog);
                     System.out.println("click ON buttonRetweetOnDialog is done...");
-
+                } else {
+                    System.out.println("Ending...");
+                    break;
                 }
             }
         }
     }
 
-    public WebElement getButtonRetweet() {
-        return buttonRetweet;
+    public Timestamp getTweetTimestamp(WebElement tweet)
+    {
+        WebElement timestampElement = tweet.findElement(By.xpath(".//span[contains(@class,'_timestamp')]"));
+        return new Timestamp(Long.parseLong(timestampElement.getAttribute("data-time-ms")));
     }
 
-    public void addElementsToList(WebElement element) {
-        listWithTweetItems.add(element);
-    }
-
-    public boolean isListEmpty() {
-        return listWithTweetItems == null;
-    }
-
-    public void printList() {
-        for (WebElement element: listWithTweetItems) {
-            System.out.println(element.toString());
-        }
-    }
-
-    public int getSizeOfList() {
-        return listWithTweetItems.size();
-    }
 }
